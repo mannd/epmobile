c3844c791a95f54b628714a66d4f9b6ee7fa68bc
David Mann <mannd@epstudiossoftware.com>
2011-08-15 21:40:14 -0400

    Added start screen menu, About EP Mobile.

---
+++ src/org/epstudios/epmobile/About.java
@@ -0,0 +1,12 @@
+package org.epstudios.epmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
--- src/org/epstudios/epmobile/EpMobile.java
+++ src/org/epstudios/epmobile/EpMobile.java
@@ -1,13 +1,46 @@
 package org.epstudios.epmobile;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.widget.Toast;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class EpMobile extends Activity {
+
+public class EpMobile extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        View dofetilideButton = findViewById(R.id.dofetilide_button);
+        dofetilideButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+    }
+    
+    public void onClick(View v) {
+    	switch (v.getId()) {
+    	case R.id.about_button:
+    		about();
+    		break;
+    	case R.id.dofetilide_button:
+    		dofetilideCalculator();
+    		break;
+    		// more buttons here
+    	}
+    }
+    	
+    
+    private void about() {
+   		Intent i = new Intent(this, About.class);
+		startActivity(i);
+    }
+    
+    private void dofetilideCalculator() {
+    	;
     }
 }
