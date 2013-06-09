f9118708cc46a9416025db569df477477a167ac9
David Mann <mannd@epstudiossoftware.com>
2013-06-09 14:33:09 -0400

    fleshing out IbwCalculator.java

--- src/org/epstudios/epmobile/IbwCalculator.java
+++ src/org/epstudios/epmobile/IbwCalculator.java
@@ -5,13 +5,32 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class IbwCalculator extends EpActivity implements OnClickListener {
+	private EditText weightEditText;
+	private EditText heightEditText;
+	private Spinner weightSpinner;
+	private Spinner heightSpinner;
+
+	private enum WeightUnit {
+		KG, LB
+	};
+
+	private enum HeightUnit {
+		CM, IN
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ibw);
+
+		weightEditText = (EditText) findViewById(R.id.weightEditText);
+		heightEditText = (EditText) findViewById(R.id.heightEditText);
+		weightSpinner = (Spinner) findViewById(R.id.weight_spinner);
+		heightSpinner = (Spinner) findViewById(R.id.height_spinner);
 
 	}
 
