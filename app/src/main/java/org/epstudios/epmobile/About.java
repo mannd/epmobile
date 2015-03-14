2337658760874d5dfb20054885e41dfdfd2bfd48
David Mann <mannd@epstudiossoftware.com>
2015-03-14 20:20:19 +0100

    bumped version to 2.10

--- app/src/main/java/org/epstudios/epmobile/About.java
+++ app/src/main/java/org/epstudios/epmobile/About.java
@@ -23,7 +23,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class About extends Activity {
-	private final static String VERSION = "2.9";
+	private final static String VERSION = "2.10";
 	private TextView versionTextView;
 
 	@Override
