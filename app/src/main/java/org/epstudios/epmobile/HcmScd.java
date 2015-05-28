672ebd85a4caab99604aa0158100100cc529629e
David Mann <mannd@epstudiossoftware.com>
2015-05-28 19:46:44 +0200

    Added ESC HCM-SCD risk calculator class and layout.

---
+++ app/src/main/java/org/epstudios/epmobile/HcmScd.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2015 EP Studios, Inc.
+ * www.epstudiossoftware.com
+ * <p/>
+ * Created by mannd on 5/28/15.
+ * <p/>
+ * This file is part of epmobile.
+ * <p/>
+ * epmobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * epmobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with epmobile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.epstudios.epmobile;
+
+public class HcmScd // extends RiskScore
+{  // TODO implement
+
+    protected String getShortReference() {
+        return "";
+    }
+}
