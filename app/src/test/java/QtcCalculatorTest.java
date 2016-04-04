a315b322d87049ef0f256b7e840b3b90ce27a6fb
David Mann <mannd@epstudiossoftware.com>
2016-04-03 18:20:00 -0600

    Add new QTc tests

--- app/src/test/java/QtcCalculatorTest.java
+++ app/src/test/java/QtcCalculatorTest.java
@@ -54,5 +54,40 @@ public class QtcCalculatorTest extends TestCase {
 		assertEquals(400, QtcCalculator.calculate(1000, 400, QtcFormula.HODGES));
 		assertEquals(444, QtcCalculator.calculate(863, 427, QtcFormula.HODGES));
 	}
-	
+
+	public void testRounding() {
+		// make sure rounding works the way we want it
+		assertTrue(Math.round(0.5) == 1);
+		assertTrue(Math.round(0.4) == 0);
+		assertTrue(Math.round(0.6) == 1);
+	}
+
+	public void testJT() {
+		assertEquals(QtcCalculator.jtInterval(398, 99), 299);
+		assertEquals(QtcCalculator.jtInterval(400, 201), 199);
+	}
+
+	public void testJTc() {
+		int jtc = QtcCalculator.jtCorrected(360, 550, 135);
+		int qtc = QtcCalculator.calculate(550, 360, QtcFormula.BAZETT);
+		assertTrue(jtc == qtc - 135);
+	}
+
+	public void testQTcLBBB() {
+		long result = QtcCalculator.qtCorrectedForLBBB(360, 144);
+		assertTrue(result == 290);
+		result = QtcCalculator.qtCorrectedForLBBB(444, 197);
+		assertTrue(result == 348);
+		result = QtcCalculator.qtCorrectedForLBBB(400, 0);
+		assertTrue(result == 400);
+	}
+
+	public void testQTcBBBSex() {
+		long result = QtcCalculator.qtRrIvcd(379, 77, 155, true);
+		assertTrue(result == 376);
+		result = QtcCalculator.qtRrIvcd(442, 55, 110, false);
+		assertTrue(result == 421);
+	}
+
+
 }
