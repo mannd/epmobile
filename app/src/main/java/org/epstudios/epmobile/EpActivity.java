9a17715e056d297a551c6f0c24a96f5b4f7ae01c
David Mann <mannd@epstudiossoftware.com>
2017-09-27 21:20:20 -0600

    Fix black text in toolbar

--- app/src/main/java/org/epstudios/epmobile/EpActivity.java
+++ app/src/main/java/org/epstudios/epmobile/EpActivity.java
@@ -23,6 +23,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.ActionBarActivity;
 
@@ -31,7 +32,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 //adds option menu functions
-public abstract class EpActivity extends ActionBarActivity {
+public abstract class EpActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
