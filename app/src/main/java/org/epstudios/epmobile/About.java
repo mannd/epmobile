e29c3ac4f66b256ba6d17cb4309a4bb670c7e804
David Mann <mannd@epstudiossoftware.com>
2018-02-21 14:27:51 -0700

    Bump version to 2.17

--- app/src/main/java/org/epstudios/epmobile/About.java
+++ app/src/main/java/org/epstudios/epmobile/About.java
@@ -23,7 +23,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class About extends Activity {
-	private final static String VERSION = "2.16";
+	private final static String VERSION = "2.17";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
