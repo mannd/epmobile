c239d1f054d55fb826fdf038d38fc09b5a8b5c08
David Mann <mannd@epstudiossoftware.com>
2025-04-24 21:08:14 +0200

    Begin implementation of Edge to Edge

---
+++ app/src/main/java/org/epstudios/epmobile/CalculatorList.kt
@@ -0,0 +1,92 @@
+package org.epstudios.epmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+
+class CalculatorList : EpActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.selectionlist)
+        initToolbar()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.my_root_view)) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = insets.top, bottom = insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
+        val adapter = ArrayAdapter.createFromResource(
+            this, R.array.calculator_list, android.R.layout.simple_list_item_1
+        )
+        val lv = findViewById<ListView>(R.id.list)
+        lv.setAdapter(adapter)
+
+        lv.setTextFilterEnabled(true)
+        lv.setOnItemClickListener(OnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+            val selection = (view as TextView).getText().toString()
+            if (selection
+                == getString(R.string.cycle_length_calculator_title)
+            ) intervalRateCalculator()
+            else if (selection
+                == getString(R.string.qtc_calculator_title)
+            ) qtcCalculator()
+            else if (selection
+                == getString(R.string.drug_dose_calculator_list_title)
+            ) drugDoseCalculators()
+            else if (selection
+                == getString(R.string.date_calculator_title)
+            ) icdDayCalculator()
+            else if (selection
+                == getString(R.string.ibw_calculator_title)
+            ) ibwCalculator()
+            else if (selection == getString(R.string.creatinine_clearance_calculator_title)) creatinineClearanceCalculator()
+            else if (selection == getString(R.string.qtc_ivcd_calculator_title)) qtcIvcdCalculator()
+            else if (selection == getString(R.string.gfr_calculator_title)) gfrCalculator()
+        })
+    }
+
+    private fun qtcCalculator() {
+        val i = Intent(this, Qtc::class.java)
+        startActivity(i)
+    }
+
+    private fun intervalRateCalculator() {
+        val i = Intent(this, CycleLength::class.java)
+        startActivity(i)
+    }
+
+    private fun drugDoseCalculators() {
+        val i = Intent(this, DrugDoseCalculatorList::class.java)
+        startActivity(i)
+    }
+
+    private fun icdDayCalculator() {
+        val i = Intent(this, DateCalculator::class.java)
+        startActivity(i)
+    }
+
+    private fun ibwCalculator() {
+        val i = Intent(this, IbwCalculator::class.java)
+        startActivity(i)
+    }
+
+    private fun creatinineClearanceCalculator() {
+        val i = Intent(this, CreatinineClearanceCalculator::class.java)
+        startActivity(i)
+    }
+
+    private fun qtcIvcdCalculator() {
+        startActivity(Intent(this, QtcIvcd::class.java))
+    }
+
+    private fun gfrCalculator() {
+        startActivity(Intent(this, GfrCalculator::class.java))
+    }
+}
---
+++ app/src/main/java/org/epstudios/epmobile/EpActivity.kt
@@ -0,0 +1,274 @@
+/*  EP Mobile -- Mobile tools for electrophysiologists
+Copyright (C) 2011 EP Studios, Inc.
+www.epstudiossoftware.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.epstudios.epmobile
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsetsController
+import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updatePadding
+
+//adds option menu functions
+abstract class EpActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        val inflater = getMenuInflater()
+        inflater.inflate(R.menu.menu, menu)
+
+        if (hideInstructionsMenuItem()) {
+            val notesItem = menu.findItem(R.id.instructions)
+            if (notesItem != null) {
+                notesItem.setVisible(false)
+            }
+        }
+        if (hideReferenceMenuItem()) {
+            val referenceItem = menu.findItem(R.id.reference)
+            if (referenceItem != null) {
+                referenceItem.setVisible(false)
+            }
+        }
+        if (hideKeyMenuItem()) {
+            val keyItem = menu.findItem(R.id.key)
+            if (keyItem != null) {
+                keyItem.setVisible(false)
+            }
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId = item.getItemId()
+        if (itemId == R.id.settings) {
+            startActivity(Intent(this, Prefs::class.java))
+            return true
+        } else if (itemId == R.id.about) {
+            startActivity(Intent(this, About::class.java))
+            return true
+        } else if (itemId == android.R.id.home) {
+            finish()
+            return true
+        } else if (itemId == R.id.instructions) {
+            if (!hideInstructionsMenuItem()) {
+                showActivityInstructions()
+            }
+        } else if (itemId == R.id.reference) {
+            if (!hideReferenceMenuItem()) {
+                showActivityReference()
+            }
+        } else if (itemId == R.id.key) {
+            if (!hideKeyMenuItem()) {
+                showActivityKey()
+            }
+        }
+        return false
+    }
+
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
+        windowInsetsController.isAppearanceLightStatusBars = false
+        windowInsetsController.isAppearanceLightNavigationBars = false
+    }
+
+    protected fun initToolbar() {
+        val toolbar = findViewById<Toolbar?>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    // Override in inherited activities to show these menu items.
+    // Default is to hide these menu items.
+    protected open fun hideInstructionsMenuItem(): Boolean {
+        return true
+    }
+
+    protected open fun hideReferenceMenuItem(): Boolean {
+        return true
+    }
+
+    protected open fun hideKeyMenuItem(): Boolean {
+        return true
+    }
+
+    // Override to inherited activities
+    protected open fun showActivityInstructions() {
+        print("showNotes should be overridden.")
+    }
+
+    protected open fun showActivityReference() {
+        print("showReference should be overridden.")
+    }
+
+    protected open fun showActivityKey() {
+        print("showKey should be overridden.")
+    }
+
+    protected fun showAlertDialog(@StringRes titleId: Int, @StringRes messageId: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(titleId)
+        builder.setMessage(messageId)
+        builder.setPositiveButton(getString(R.string.ok_button_label), null)
+        val alert = builder.create()
+        alert.show()
+    }
+
+    protected fun showAlertDialog(title: String?, message: Spanned?) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(getString(R.string.ok_button_label), null)
+        val alert = builder.create()
+        alert.show()
+        (alert.findViewById<View?>(android.R.id.message) as TextView).setMovementMethod(
+            LinkMovementMethod.getInstance()
+        )
+    }
+
+    protected fun showAlertDialog(title: String?, message: String?) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(getString(R.string.ok_button_label), null)
+        val alert = builder.create()
+        alert.show()
+    }
+
+    protected fun showReferenceAlertDialog(
+        @StringRes referenceId: Int,
+        @StringRes linkId: Int
+    ) {
+        val html = convertReferenceToHtml(referenceId, linkId)
+        if (html != null) {
+            showAlertDialog(getString(R.string.reference_label), html)
+        } else {
+            showAlertDialog(
+                getString(R.string.error_dialog_title),
+                getString(R.string.error_message)
+            )
+        }
+    }
+
+    protected fun showReferenceAlertDialog(references: Array<Reference>) {
+        val html = convertReferencesToHtml(references)
+        if (html != null) {
+            showAlertDialog(getString(R.string.references_label), html)
+        } else {
+            showAlertDialog(
+                getString(R.string.error_dialog_title),
+                getString(R.string.error_message)
+            )
+        }
+    }
+
+    protected fun showKeyAlertDialog(@StringRes keyId: Int) {
+        showAlertDialog(R.string.key_label, keyId)
+    }
+
+    fun convertReferenceToHtml(
+        @StringRes referenceId: Int,
+        @StringRes linkId: Int
+    ): Spanned? {
+        val reference = getString(referenceId)
+        val link = getString(linkId)
+        // Only forbidden combo is reference == null.  Can have a null
+        // link if the paper is old.
+        if (reference != null) {
+            val htmlString: String = convertReferenceToHtmlString(reference, link)
+            return Html.fromHtml(htmlString)
+        } else {
+            return null
+        }
+    }
+
+    fun convertReferenceToText(
+        @StringRes referenceId: Int,
+        @StringRes linkId: Int
+    ): String? {
+        val reference = getString(referenceId)
+        val link = getString(linkId)
+        if (reference != null) {
+            val referencePlusLink = reference + " " + link
+            return referencePlusLink
+        }
+        return null
+    }
+
+    // Handle multiple references.
+    fun convertReferencesToHtml(references: Array<Reference>): Spanned? {
+        val htmlString: String? = convertReferencesToHtmlString(references)
+        if (htmlString == null) {
+            return null
+        } else {
+            return Html.fromHtml(htmlString)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun convertReferenceToHtmlString(
+            reference: String,
+            link: String?
+        ): String {
+            var html = ""
+            if (link != null) {
+                html = "<p>" + reference +
+                        "<br/><a href =\"" +
+                        link + "\">Link to reference</a></p>"
+            } else {
+                html = "<p>" + reference + "<br/><i>No link available</i></p>"
+            }
+            return html
+        }
+
+        fun convertReferencesToHtmlString(references: Array<Reference>): String? {
+            var htmlString = ""
+            for (reference in references) {
+                // Only forbidden combo is reference == null.  Can have a null
+                // link if the paper is old.
+                if (reference.getText() != null) {
+                    htmlString += convertReferenceToHtmlString(
+                        reference.getText(),
+                        reference.getLink()
+                    )
+                } else {
+                    return null
+                }
+            }
+            return htmlString
+        }
+    }
+}
